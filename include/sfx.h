0854cf5a8a64ad70cc2fadbaff080e18634c8958
Kenny D. Lee <kennyd.lee@outlook.com>
2015-10-06 14:38:49 -0400

    Update sfx.h

--- include/sfx.h
+++ include/sfx.h
@@ -5,18 +5,20 @@
 
 typedef struct
 {
-	u8* data;
+	u8* rdata;
+	u8* ldata;
 	u32 size;
 	u32 format;
+	u32 sampleRate; //44100, 22050, 11025
+	bool stereo;
 	bool used;
 }SFX_s;
 
 void initSound(void);
 void exitSound(void);
 void initSFX(SFX_s* s);
-void loadSFX(SFX_s* s, char* filename, u32 format);
-SFX_s* createSFX(char* filename, u32 format);
+void loadSFX(SFX_s* s, char* filename, u32 format, u32 Rate, bool InStereo);
+SFX_s* createSFX(char* filename, u32 format, u32 Rate, bool InStereo);
 void playSFX(SFX_s* s);
-void playPCM(SFX_s* s);
 
 #endif
