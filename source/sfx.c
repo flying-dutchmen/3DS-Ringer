b81599dbcb94ce2f7e33489f4e1f12c3808ec815
Kenny D. Lee <kennyd.lee@outlook.com>
2015-10-06 14:37:58 -0400

    Update sfx.c

--- source/sfx.c
+++ source/sfx.c
@@ -27,10 +27,15 @@ void exitSound(void)
 	{
 		if(SFX[i].used)
 		{
-			if(SFX[i].data)
+			if(SFX[i].ldata)
 			{
-				linearFree(SFX[i].data);
-				SFX[i].data=NULL;
+				linearFree(SFX[i].ldata);
+				SFX[i].ldata=NULL;
+			}			
+                        if ((SFX[i].stereo) && (SFX[i].rdata))
+			{
+				linearFree(SFX[i].rdata);
+				SFX[i].rdata=NULL;
 			}
 			SFX[i].used=false;
 		}
@@ -41,32 +46,62 @@ void exitSound(void)
 void initSFX(SFX_s* s)
 {
 	if(!s)return;
-
-	s->data=NULL;
+	s->ldata=NULL;
+	s->rdata=NULL;
 	s->size=0;
 	s->used=true;
+	s->stereo=false;
 }
 
-void loadSFX(SFX_s* s, char* filename, u32 format)
+//to be tested
+void deinterlaceSFX(SFX_s* s, char* filename)
+{
+	if(!s)return;
+        int i;
+	u8* data = bufferizeFile(filename, &s->size, true, true); //read all data 
+        s->ldata=(u8*)linearMemAlign(s->size >> 1, 0x80); //setup left
+        s->rdata=(u8*)linearMemAlign(s->size >> 1, 0x80); //setup right
+
+        // [left][right] ...
+	if (s->format = SOUND_FORMAT_8BIT)
+        for(i=0;i<s->size;i+=2)
+	{
+          s->ldata[i] = data[i]; 
+          s->rdata[i] = data[i+1];
+        } else //16bit 
+        for(i=0;i<s->size;i+=4)
+	{
+          s->ldata[i] = data[i];
+          s->rdata[i] = data[i+1];
+          s->ldata[i+1] = data[i+2];
+          s->rdata[i+1] = data[i+3];
+        };
+
+//reset size & free buffer
+        s->size = s->size >> 1; // div 2
+        linearFree(data);
+}
+
+void loadSFX(SFX_s* s, char* filename, u32 format, u32 Rate, bool inStereo)
 {
 	if(!s)return;
 
 	initSFX(s);
-
-	s->data=bufferizeFile(filename, &s->size, true, true);
 	s->format=format;
+	s->sampleRate=Rate;
+	s->stereo=inStereo;
+	(!s->stereo) ? s->ldata=bufferizeFile(filename, &s->size, true, true) : deinterlaceSFX(s, filename);
 }
 
-SFX_s* createSFX(char* filename, u32 format)
+SFX_s* createSFX(char* filename, u32 format, u32 Rate, bool inStereo)
 {
 	int i;
 	for(i=0;i<NUMSFX;i++)
 	{
 		if(!SFX[i].used)
 		{
-			loadSFX(&SFX[i], filename, format);
-
-			if(!SFX[i].data)return NULL;
+			loadSFX(&SFX[i], filename, format, Rate, inStereo);
+			if(!SFX[i].ldata)return NULL;
 			return &SFX[i];
 		}
 	}
@@ -77,22 +112,11 @@ int channel;
 
 void playSFX(SFX_s* s)
 {
-	if(!s || !s->used || !s->data || !soundEnabled)return;
+	if(!s || !s->used || !s->ldata || !soundEnabled)return;
 
 	channel++;
 	channel%=8;
 
-	// soundPlaySample(s->data, s->format, s->size, 22050, 127, 64, false, 0);
-	csndPlaySound(channel+8, s->format, 22050, 1.0, 0.0, (u32*)s->data, (u32*)s->data, s->size);
-}
-
-void playPCM(SFX_s* s)
-{
-	if(!s || !s->used || !s->data || !soundEnabled)return;
-
-	channel++;
-	channel%=8;
-
-	// soundPlaySample(s->data, s->format, s->size, 22050, 127, 64, false, 0);
-	csndPlaySound(channel+8, s->format, 44100, 1.0, 0.0, (u32*)s->data, (u32*)s->data, s->size);
+          (!s->stereo) ? csndPlaySound(channel+8, s->format, s->sampleRate, 1.0, 0.0, (u32*)s->ldata, (u32*)s->ldata, s->size):
+          csndPlaySound(channel+8, s->format, s->sampleRate, 1.0, 0.0, (u32*)s->ldata, (u32*)s->rdata, s->size);
 }
